19f1c3067c35f5928062c47b9bd43ea7241ee87f
Ronaiza Cardoso <ronaiza.cardoso@luizalabs.com>
2021-10-26 14:48:43 -0300

    feat: add gh pages

--- pages/_app.js
+++ pages/_app.js
@@ -6,16 +6,11 @@ import GoogleTagManagerHead from '../components/GoogleTagManagerHead'
 import GoogleTagManagerNoscript from '../components/GoogleTagManagerNoscript'
 
 function MyApp({ Component, pageProps }) {
-  console.log('process.env :>> ', process.env.NEXT_PUBLIC_ANALYTICS_ID)
   return (
     <>
       <Head>
-        <GoogleTagManagerHead
-          containerId={process.env.NEXT_PUBLIC_ANALYTICS_ID}
-        />
-        <GoogleTagManagerNoscript
-          containerId={process.env.NEXT_PUBLIC_ANALYTICS_ID}
-        />
+        <GoogleTagManagerHead containerId={process.env.NEXT_PUBLIC_GTM} />
+        <GoogleTagManagerNoscript containerId={process.env.NEXT_PUBLIC_GTM} />
         <Partytown
           debug
           logCalls
