d4db41b978a3ab0ec29fc8f5d3f3d324de22e8bb
Ronaiza Cardoso <ronaiza.cardoso@luizalabs.com>
2021-10-29 09:52:47 -0300

    trying it out with vanilla script

--- pages/_app.js
+++ pages/_app.js
@@ -6,21 +6,29 @@ import GoogleTagManagerHead from '../components/GoogleTagManagerHead'
 import GoogleTagManagerNoscript from '../components/GoogleTagManagerNoscript'
 
 function MyApp({ Component, pageProps }) {
+  const partytown = {
+    debug: true,
+    forward: [['dataLayer', 1]],
+    logCalls: true,
+    logGetters: true,
+    logSetters: true,
+    logImageRequests: true,
+    logMainAccess: true,
+    logSendBeaconRequests: true,
+    logStackTraces: false,
+    logScriptExecution: true
+  }
   return (
     <>
       <Head>
         <GoogleTagManagerHead containerId={process.env.NEXT_PUBLIC_GTM} />
         <GoogleTagManagerNoscript containerId={process.env.NEXT_PUBLIC_GTM} />
-        <Partytown
-          debug
-          logCalls
-          logGetters
-          logSetters
-          logImageRequests
-          logScriptExecution
-          logSendBeaconRequests
-          logStackTraces
+        <script
+          dangerouslySetInnerHTML={{
+            __html: partytown
+          }}
         />
+        <script async src="/~partytown/debug/partytown-snippet.js" />
       </Head>
       <Component {...pageProps} />
     </>
